5280b3f13097f8a62473e4593ab5c61638eabc3d
marko streleshki <marko_streleshki_96@abv.bg>
2020-04-09 21:43:35 +0300

    Navbar components with styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaAlignRight } from 'react-icons/fa'
 import logo from '../../mocks/images/logo.svg';
+import './Navbar.css'
 
 export default class Navbar extends React.Component {
   state = {
@@ -13,8 +14,29 @@ export default class Navbar extends React.Component {
   }
   render() {
     return (
-      <nav>
-        Navbar
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src={logo} alt="logoImage"/>
+            </Link>
+            <button 
+              type="button" 
+              className="nav-btn" 
+              onClick={() => this.handleToggle()}
+            >
+              <FaAlignRight className="nav-icon" />
+            </button>
+          </div>
+          <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Rooms</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     )
   }
